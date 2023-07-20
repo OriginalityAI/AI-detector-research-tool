dd0ddea4a0c049ad2c19aa3fab8cef03c2eba33e
James Ball <jamie.j.ball@gmail.com>
2023-07-20 11:26:37 -0700

    feat: create main file to run functions

---
+++ main.py
@@ -0,0 +1,7 @@
+from text_analyzer import text_analyzer_main
+from analyze_output import csv_analyzer_main
+
+
+# output_csv = text_analyzer_main()
+output_csv = "test_output.csv"
+csv_analyzer_main(output_csv)
