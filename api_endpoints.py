8c14c0a78441aa39ad007de68f4da8bd28bbf51f
James Ball <jamie.j.ball@gmail.com>
2023-07-21 11:18:41 -0700

    comment out unused API's

--- api_endpoints.py
+++ api_endpoints.py
@@ -58,38 +58,38 @@ API_ENDPOINTS = {
         },
         "response": {"200": {"documents": ["completely_generated_prob"]}},
     },
-    "ZeroGPT": {
-        "post_parameters": {
-            "endpoint": "https://zerogpt.p.rapidapi.com/api/v1/detectText",
-            "body": {"input_text": "Sample"},
-            "headers": {"X-RapidAPI-Key": "", "Content-Type": "application/json"},
-            "API_KEY_POINTER": {
-                "location": "headers",
-                "value": "",
-                "key_name": "X-RapidAPI-Key",
-            },
-            "text_key": "input_text",
-        },
-        "response": {
-            "200": {
-                "is_human_written": "",
-                "is_gpt_generated": "",
-            }
-        },
-    },
-    "Copyleaks": {
-        "post_parameters": {
-            "endpoint": "https://api.copyleaks.com/v2/writer-detector/{copyLeaks_scan_id}/check",
-            "headers": {},
-            "body": {"text": "Sample"},
-            "API_KEY_POINTER": {
-                "location": "headers",
-                "value": "",
-                "key_name": "Authorization",
-            },
-            "text_key": "text",
-        },
-        "response": {"200": {"summary": [{"ai": "ai", "human": "human"}]}},
-    }
+    # "ZeroGPT": {
+    #     "post_parameters": {
+    #         "endpoint": "https://zerogpt.p.rapidapi.com/api/v1/detectText",
+    #         "body": {"input_text": "Sample"},
+    #         "headers": {"X-RapidAPI-Key": "", "Content-Type": "application/json"},
+    #         "API_KEY_POINTER": {
+    #             "location": "headers",
+    #             "value": "",
+    #             "key_name": "X-RapidAPI-Key",
+    #         },
+    #         "text_key": "input_text",
+    #     },
+    #     "response": {
+    #         "200": {
+    #             "is_human_written": "",
+    #             "is_gpt_generated": "",
+    #         }
+    #     },
+    # },
+    # "Copyleaks": {
+    #     "post_parameters": {
+    #         "endpoint": "https://api.copyleaks.com/v2/writer-detector/{copyLeaks_scan_id}/check",
+    #         "headers": {},
+    #         "body": {"text": "Sample"},
+    #         "API_KEY_POINTER": {
+    #             "location": "headers",
+    #             "value": "",
+    #             "key_name": "Authorization",
+    #         },
+    #         "text_key": "text",
+    #     },
+    #     "response": {"200": {"summary": [{"ai": "ai", "human": "human"}]}},
+    # }
     # Add more endpoints as needed
 }
