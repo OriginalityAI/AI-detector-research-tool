0f0af43c4e5450e2a3fdf68a947cd252f65eac44
James Ball <jamie.j.ball@gmail.com>
2023-07-20 17:11:48 -0700

    feat: get user input

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from analyze_output import csv_analyzer_main
 
 # output_csv = text_analyzer_main()
 output_csv = "test_output.csv"
-csv_analyzer_main(output_csv)
+graphs = input("Would you like to generate a confusion matrix? (y/n): ")
+if graphs == "y":
+    csv_analyzer_main(output_csv)
