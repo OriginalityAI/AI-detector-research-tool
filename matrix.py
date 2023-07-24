0b80e02e01760eaab799eff4ce47e9ebb554d4b7
James Ball <jamie.j.ball@gmail.com>
2023-07-24 14:54:13 -0700

    feat: runnable file to build matrix

---
+++ matrix.py
@@ -0,0 +1,10 @@
+from analyze_output import csv_analyzer_main
+
+input_csv = input("Enter the name of the csv file to analyze: ")
+if not input_csv.endswith(".csv"):
+    input_csv += ".csv"
+try:
+    csv_analyzer_main(input_csv)
+except Exception as e:
+    print(f"An error occurred: {e}")
+input("Press enter to exit...")
