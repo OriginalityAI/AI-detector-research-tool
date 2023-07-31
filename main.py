bd195cae1c2a9bd991ec4644db4c6f2335c103fa
James Ball <jamie.j.ball@gmail.com>
2023-07-31 06:43:37 -0700

    fix: More robust user input

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from analyze_output import csv_analyzer_main
 
 output_csv = text_analyzer_main()
 graphs = input("Would you like to generate a confusion matrix? (y/n): ")
-if graphs == "y":
+if graphs.lower() == "y":
     try:
         csv_analyzer_main(output_csv)
+        print("Done!")
     except Exception as e:
         print(f"An error occurred: {e}")
 input("Press enter to exit...")
